2279711eb6a1af749f5c64605e4fe0d72d5caacc
Adam Hawke <ah14275@my.bristol.ac.uk>
2023-08-06 21:16:11 +0100

    Added code for displaying the call log history

--- app/src/main/java/com/codersguidebook/communication/ui/callLog/CallLogAdapter.kt
+++ app/src/main/java/com/codersguidebook/communication/ui/callLog/CallLogAdapter.kt
@@ -6,11 +6,15 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.codersguidebook.communication.CallLogEvent
 import com.codersguidebook.communication.MainActivity
 import com.codersguidebook.communication.R
 
 class CallLogAdapter(private val activity: MainActivity) : RecyclerView.Adapter<CallLogAdapter.CallLogViewHolder>() {
+
+    var callLog = listOf<CallLogEvent>()
 
     inner class CallLogViewHolder(itemView: View) :
         RecyclerView.ViewHolder(itemView) {
@@ -31,4 +35,30 @@ class CallLogAdapter(private val activity: MainActivity) : RecyclerView.Adapter<
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CallLogViewHolder {
         return CallLogViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.call_log_entry, parent, false))
     }
+
+    override fun onBindViewHolder(holder: CallLogViewHolder, position: Int) {
+        val current = callLog[position]
+
+        val callDirection = holder.direction
+        callDirection.visibility = View.VISIBLE
+        callDirection.setColorFilter(ContextCompat.getColor(activity, android.R.color.holo_green_dark))
+        when (current.direction) {
+            "OUTGOING" -> callDirection.setImageResource(R.drawable.ic_outgoing)
+            "INCOMING" -> callDirection.setImageResource(R.drawable.ic_incoming)
+            "MISSED" -> {
+                callDirection.setImageResource(R.drawable.ic_missed)
+                callDirection.setColorFilter(ContextCompat.getColor(activity, android.R.color.holo_red_dark))
+            }
+            null -> callDirection.visibility = View.INVISIBLE
+        }
+
+        holder.phoneNumber.text = current.number
+        holder.callDate.text = current.date
+
+        holder.callback.setOnClickListener {
+            if (current.number.isNotBlank()) activity.callNumber(current.number)
+        }
+    }
+
+    override fun getItemCount() = callLog.size
 }
--- app/src/main/java/com/codersguidebook/communication/ui/callLog/CallLogFragment.kt
+++ app/src/main/java/com/codersguidebook/communication/ui/callLog/CallLogFragment.kt
@@ -1,15 +1,20 @@
 package com.codersguidebook.communication.ui.callLog
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.codersguidebook.communication.CommunicationViewModel
 import com.codersguidebook.communication.MainActivity
 import com.codersguidebook.communication.databinding.FragmentCallLogBinding
 
 class CallLogFragment : Fragment() {
 
+    private val communicationViewModel: CommunicationViewModel by activityViewModels()
     private var _binding: FragmentCallLogBinding? = null
     private lateinit var mainActivity: MainActivity
     private lateinit var adapter: CallLogAdapter
@@ -31,6 +36,21 @@ class CallLogFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.root.layoutManager = LinearLayoutManager(activity)
+        binding.root.adapter = adapter
+
+        mainActivity.getCallLogs()
+
+        communicationViewModel.callLog.observe(viewLifecycleOwner) { log ->
+            // Load the 10 most recent calls
+            adapter.callLog = log.take(10)
+            adapter.notifyDataSetChanged()
+        }
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
